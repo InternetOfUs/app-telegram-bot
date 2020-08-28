8012ef00a48921a504c520008ae402ce8dc9bcfd
Carlo Caprini <carlo.caprini@u-hopper.com>
2020-08-28 10:06:25 +0200

    Updated ci pipeline

--- docker-support/runner.sh
+++ docker-support/runner.sh
@@ -14,12 +14,12 @@ PUSH_IMAGE=0
 args=`getopt btdsp $*`
 # you should not use `getopt abo: "$@"` since that would parse
 # the arguments differently from what the set command below does.
-if [ $? != 0 ]
+if [[ $? != 0 ]]
 then
   echo 'Usage: ...'
   exit 2
 fi
-set -- $args
+set -- ${args}
 # You cannot use the set command with a backquoted getopt directly,
 # since the exit code from getopt would be shadowed by those of set,
 # which is zero by definition.
@@ -45,7 +45,7 @@ done
 
 # Identifying build version
 VERSION=$2
-if [ -z "${VERSION}" ]; then
+if [[ -z "${VERSION}" ]]; then
     VERSION=${DEFAULT_VERSION}
     echo "Version not specified: building with default version [${VERSION}]"
 else
@@ -58,28 +58,28 @@ export IMAGE_NAME=wenet/bots:${VERSION}
 export REGISTRY=${REGISTRY}
 
 
-if [ $BUILD == 1 ]; then
+if [[ ${BUILD} == 1 ]]; then
   echo "Building image"
   ${SCRIPT_DIR}/build_image.sh
 
-  if [ $? != 0 ]; then
+  if [[ $? != 0 ]]; then
     echo "Build Failed"
     exit 1
   fi
 
-  if [ $SAVE_IMAGE_TO_TARGZ == 1 ]; then
+  if [[ ${SAVE_IMAGE_TO_TARGZ} == 1 ]]; then
     echo "Saving image to bots_image.tar.gz"
     docker save ${REGISTRY}/${IMAGE_NAME} | gzip > bots_image.tar.gz
   fi
 fi
 
-if [ $TEST == 1 ]; then
+if [[ ${TEST} == 1 ]]; then
   echo "Running tests"
   ${SCRIPT_DIR}/test.sh
 
-  if [ $? != 0 ]; then
+  if [[ $? != 0 ]]; then
       echo "Error: One or more tests are failing"
-      if [ $DELETE_IF_FAILED == 1 ]; then
+      if [[ ${DELETE_IF_FAILED} == 1 ]]; then
         docker rmi ${IMAGE_NAME}
       fi
       exit 1
@@ -87,12 +87,12 @@ if [ $TEST == 1 ]; then
 
 fi
 
-if [ $PUSH_IMAGE == 1 ]; then
+if [[ ${PUSH_IMAGE} == 1 ]]; then
   echo "Pushing image to registry"
   docker push ${REGISTRY}/${IMAGE_NAME}
 fi
 
-if [ $BUILD == 0 ] && [ $TEST == 0 ] && [ $PUSH_IMAGE == 0 ]; then
+if [[ ${BUILD} == 0 ]] && [[ ${TEST} == 0 ]] && [[ ${PUSH_IMAGE} == 0 ]]; then
   echo "Need to specify at least one parameter (-b, -t, -p)"
   exit 1
 fi
