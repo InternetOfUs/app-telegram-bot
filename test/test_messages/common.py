e921cc35a3f29590bbda5672ad87ce685d8e3bee
Nicolò Pomini <nicolo.pomini@u-hopper.com>
2020-10-07 10:31:50 +0200

    Fixed broken tests, since the constructor of the MessageInterface was changed in the past

--- test/test_messages/common.py
+++ test/test_messages/common.py
@@ -8,7 +8,7 @@ class CommonTestCase(TestCase):
 
     def setUp(self) -> None:
         self.mqtt_publisher = MockMqttPublishHandler()
-        api = MessageInterface(self.mqtt_publisher, "topic", "namespace", "test")
+        api = MessageInterface(self.mqtt_publisher, "topic", "namespace", "test", "", "")
         api.get_application().testing = True
         self.client = api.get_application().test_client()
         super().setUp()
